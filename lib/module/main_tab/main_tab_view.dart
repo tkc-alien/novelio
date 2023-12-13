14f1a193485048f16efdecf3c384166cc9f28c16
kouhei-takashima <kouhei-takashima@dip-net.co.jp>
2023-12-13 13:00:51 +0900

    メインタブナビゲーションを作成

---
+++ lib/module/main_tab/main_tab_view.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+/// メインタブナビゲーション
+class MainTabView extends ConsumerStatefulWidget {
+  const MainTabView({super.key});
+
+  @override
+  ConsumerState createState() => _MainTabViewState();
+}
+
+class _MainTabViewState extends ConsumerState<MainTabView> {
+  /// タブのルート画面リスト
+  static const tabViews = [
+    Scaffold(), // TODO implement
+    Scaffold(), // TODO implement
+  ];
+
+  /// タブごとのナビゲーションキー
+  final navKeys = List.generate(tabViews.length, (_) => GlobalKey<NavigatorState>());
+
+  /// コントローラ
+  final controller = CupertinoTabController();
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: onWillPop,
+      child: CupertinoTabScaffold(
+        controller: controller,
+        resizeToAvoidBottomInset: false,
+        tabBar: CupertinoTabBar(
+          items: const [
+            BottomNavigationBarItem(icon: Icon(CupertinoIcons.search)),
+            BottomNavigationBarItem(icon: Icon(CupertinoIcons.book)),
+          ],
+        ),
+        tabBuilder: (context, index) => CupertinoTabView(
+          navigatorKey: navKeys[index],
+          builder: (context) => tabViews[index],
+        ),
+      ),
+    );
+  }
+
+  /// 戻る動作のハンドラ
+  Future<bool> onWillPop() async {
+    // 表示中のタブをポップする
+    final key = navKeys[controller.index].currentState;
+    if (key != null && key.canPop()) key.pop();
+    return false;
+  }
+}
